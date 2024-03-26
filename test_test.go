d5b2a56443e8b87c0cacc9cc025f4ec167ff45a3
Stefano Scarpitta <ste.scarpitta@gmail.com>
2024-03-26 09:15:49 +0100

    added a basic test

--- test_test.go
+++ test_test.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestMyCode(t *testing.T) {
-	// t.Fatalf("There is nothing tested here!!!!")
+	body := []byte("{ \"nextEPSDate\":\"2024-01-01\" }")
+	message := formatResp(body, "AAPL")
+
+	control_message := "AAPL\t --> \t2024-01-01\n"
+
+	if strings.Compare(message, control_message) != 0 {
+		t.Log("Expected:\t AAPL\t --> \t2024-01-01")
+		t.Logf("Got:\t %v", message)
+		t.Fatalf("incorrect formatting on formatResp func")
+	}
 }
