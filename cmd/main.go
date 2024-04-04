491a842318f18f64f4958a0e285ee05ce5e174f8
Stefano Scarpitta <ste.scarpitta@gmail.com>
2024-04-04 15:24:01 +0200

    removed unused files and updated stuff

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"html/template"
 	"io"
-	"net/http"
-	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -21,14 +18,13 @@ func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Co
 
 func newTemplate() *Templates {
 	return &Templates{
-		templates: template.Must(template.ParseGlob("../views/*.html")),
+		templates: template.Must(template.ParseGlob("./views/*.html")),
 	}
 }
 
 // The struct with the .Count property we refere to in the index.html
-type Release struct {
-	Symbol string
-	ERD    string
+type Count struct {
+	Count int
 }
 
 func main() {
@@ -36,49 +32,54 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	release := Release{Symbol: "AAPL"}
+	count := Count{Count: 0}
 	e.Renderer = newTemplate()
+
 	e.GET("/", func(c echo.Context) error {
-		release.ERD = getNextEPS(release.Symbol, &http.Client{})
-		return c.Render(200, "index", release)
+		return c.Render(200, "index", count)
+	})
+
+	e.POST("/count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "index", count)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
 
 }
 
-func getNextEPS(symbol string, client *http.Client) string {
-	// preparing request
-	pathElems := []string{"https://www.earningswhispers.com/api/getstocksdata/", symbol}
-	path := strings.Join(pathElems, "")
-	req, _ := http.NewRequest("GET", path, nil)
-	req.Header.Add("Accept", "application/json, text/javascript, */*; q=0.01")
-	req.Header.Add("Accept-Language", "en-US,en;q=0.5")
-	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Add("X-Requested-With", "XMLHttpRequest")
-	req.Header.Add("Referer", "https://www.earningswhispers.com/stocks/STEM")
-
-	// performing http req
-	resp, _ := client.Do(req)
-
-	defer resp.Body.Close()
-
-	// reading body as bytes
-	body, _ := io.ReadAll(resp.Body)
-
-	return formatResp(body, symbol)
-}
-
-func formatResp(body []byte, symbol string) (result string) {
-	var j interface{}
-	json.Unmarshal(body, &j)
-
-	// converting json to map
-	m := j.(map[string]interface{})
-
-	// printing needed value from map
-	// only up to char 10 to remove the timestamp
-	mess := []string{symbol, "\t --> \t", m["nextEPSDate"].(string)[0:10], "\n"}
-	result = strings.Join(mess, "")
-	return
-}
+// func getNextEPS(symbol string, client *http.Client) string {
+// 	// preparing request
+// 	pathElems := []string{"https://www.earningswhispers.com/api/getstocksdata/", symbol}
+// 	path := strings.Join(pathElems, "")
+// 	req, _ := http.NewRequest("GET", path, nil)
+// 	req.Header.Add("Accept", "application/json, text/javascript, */*; q=0.01")
+// 	req.Header.Add("Accept-Language", "en-US,en;q=0.5")
+// 	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+// 	req.Header.Add("X-Requested-With", "XMLHttpRequest")
+// 	req.Header.Add("Referer", "https://www.earningswhispers.com/stocks/STEM")
+//
+// 	// performing http req
+// 	resp, _ := client.Do(req)
+//
+// 	defer resp.Body.Close()
+//
+// 	// reading body as bytes
+// 	body, _ := io.ReadAll(resp.Body)
+//
+// 	return formatResp(body, symbol)
+// }
+//
+// func formatResp(body []byte, symbol string) (result string) {
+// 	var j interface{}
+// 	json.Unmarshal(body, &j)
+//
+// 	// converting json to map
+// 	m := j.(map[string]interface{})
+//
+// 	// printing needed value from map
+// 	// only up to char 10 to remove the timestamp
+// 	mess := []string{symbol, "\t --> \t", m["nextEPSDate"].(string)[0:10], "\n"}
+// 	result = strings.Join(mess, "")
+// 	return
+// }
